e4c9f184b931de413a144550bc1d5f762c9d9b21
Semih Türkü <semihturku5252@gmail.com>
2024-07-08 22:39:18 +0300

    mini-projectr

---
+++ password_manager.py
@@ -0,0 +1,24 @@
+def view():
+    with open("password.txt", "r") as f:
+        for line in f.readlines():
+            print(line.rstrip())
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open("password.txt", "a") as f:
+        f.write(name + " | " + pwd + "\n")
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit ? ").lower()
+
+    if mode == "q":
+        break
+    elif mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid mode.")
+        continue
